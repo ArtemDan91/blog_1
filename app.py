649dbc374e1a958c1f56cf028f880a3ae864baf8
ArtemDan <artem.danylenko1991@gmail.com>
2023-03-18 22:22:11 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, url_for, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from time import sleep
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = 'asfg34refhjghmnvcbcvx'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Article %r>" % self.id
+
+# with app.app_context():
+#     db.create_all()
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/posts') #виведення усіх даних із бази з подальшою обробкою через for в html документі
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+@app.route('/posts/<int:id>') #взяття одного стовпця з бази(у нашому випадку id статті)
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+
+@app.route('/posts/<int:id>/delete')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except Exception as e:
+        print("Помилка додавання статті у базу:", e)
+        return 'При видаленні статті виникла помилка'
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            if article:
+                return redirect('/posts')
+        except Exception as e:
+            print("Помилка додавання статті у базу:", e)
+            return 'При редагуванні статті виникла помилка'
+    return render_template('post_update.html', article=article)
+
+@app.route('/create_article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            with app.app_context():
+                db.session.add(article)
+                db.session.commit()
+            if article:
+                # flash("Стаття успішно додана")
+                return redirect('/posts')
+        except Exception as e:
+            print("Помилка додавання статті у базу:", e)
+            return 'При доданні статті виникла помилка'
+    return render_template('create_article.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
